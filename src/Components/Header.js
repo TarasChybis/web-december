2088ccaf361b9f3db4ec98de4c5db46f37a9e89b
Taras Chybis <t0660804676@gmail.com>
2024-12-29 06:45:09 +0200

    creating static blog page

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -38,20 +38,20 @@ const Header = () => {
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
+            <Nav className="mr-auto" style={{gap: "30px", marginLeft: "30px"}}>
               <Nav.Link href="/">Home</Nav.Link>
               <Nav.Link href="/about">About us</Nav.Link>
               <Nav.Link href="/contacts">Contacts</Nav.Link>
               <Nav.Link href="/blog">Blog</Nav.Link>
             </Nav>
-            <Form inline>
+            <Form inline style={{marginLeft: "400px"}}>
               <FormControl
                 type="text"
                 placeholder="Search"
                 className="mr-sm-2"
               />
             </Form>
-            <Button variant="outline-info">Search</Button>
+            <Button variant="outline-info" style={{marginLeft: "30px"}}>Search</Button>
           </Navbar.Collapse>
         </Container>
       </Navbar>
--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,7 +1,92 @@
 import React from "react";
+import { Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import JsLogo from "../Assets/Unofficial_JavaScript_logo_2.svg.jpg";
 
 const Blog = () => {
-  return <h2>Blog</h2>;
+   return (
+      <Container>
+         <Row>
+            <Col md="9">
+               <div className="d-flex align-items-center m-5">
+                  <div className="flex-shrink-0">
+                     <img width={150}
+                        height={150}
+                        className="mr-3"
+                        src={JsLogo}
+                        alt="JsLogo"
+                     />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                     <h5>Blog post</h5>
+                     <p>This is some content from media-component. You can change it to any content and adjust it if necessary.</p>
+                  </div>
+               </div>
+               <div className="d-flex align-items-center m-5">
+                  <div className="flex-shrink-0">
+                     <img width={150}
+                        height={150}
+                        className="mr-3"
+                        src={JsLogo}
+                        alt="JsLogo"
+                     />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                     <h5>Blog post</h5>
+                     <p>This is some content from media-component. You can change it to any content and adjust it if necessary.</p>
+                  </div>
+               </div>
+               <div className="d-flex align-items-center m-5">
+                  <div className="flex-shrink-0">
+                     <img width={150}
+                        height={150}
+                        className="mr-3"
+                        src={JsLogo}
+                        alt="JsLogo"
+                     />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                     <h5>Blog post</h5>
+                     <p>This is some content from media-component. You can change it to any content and adjust it if necessary.</p>
+                  </div>
+               </div>
+               <div className="d-flex align-items-center m-5">
+                  <div className="flex-shrink-0">
+                     <img width={150}
+                        height={150}
+                        className="mr-3"
+                        src={JsLogo}
+                        alt="JsLogo"
+                     />
+                  </div>
+                  <div className="flex-grow-1 ms-3">
+                     <h5>Blog post</h5>
+                     <p>This is some content from media-component. You can change it to any content and adjust it if necessary.</p>
+                  </div>
+               </div>
+            </Col>
+            <Col md="3">
+               <h5 className="text-center mt-5">Categories</h5>
+               <Card>
+                  <ListGroup variant="flush">
+                     <ListGroup.Item>HTML/CSS</ListGroup.Item>
+                     <ListGroup.Item>JavaScript</ListGroup.Item>
+                     <ListGroup.Item>Python</ListGroup.Item>
+                     <ListGroup.Item>Java</ListGroup.Item>
+                     <ListGroup.Item>C++</ListGroup.Item>
+                  </ListGroup>
+               </Card>
+               <Card className="mt-3" bg="light">
+                  <Card.Body>
+                     <Card.Title>Side widget </Card.Title>
+                     <Card.Text>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id doloribus explicabo, totam neque placeat voluptate.</p>
+                     </Card.Text>
+                  </Card.Body>
+               </Card>
+            </Col>
+         </Row>
+     </Container>
+  );
 };
 
 export default Blog;
